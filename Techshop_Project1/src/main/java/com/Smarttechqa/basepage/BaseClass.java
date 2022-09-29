be2bbe4872706987b1152217eaac9ac7236447cb
fatim <fatim@10.0.0.3>
2022-09-29 17:38:45 -0400

    My second commit

--- Techshop_Project1/src/main/java/com/Smarttechqa/basepage/BaseClass.java
+++ Techshop_Project1/src/main/java/com/Smarttechqa/basepage/BaseClass.java
@@ -16,13 +16,19 @@ public class BaseClass {
 		driver.manage().window().maximize();
 		driver.get("https://www.smarttechqa.com/login");
 		
-		driver.findElement(By.id("email")) .sendKeys("Ftestuser@email.com");
-		driver.findElement (By.id("password")) .sendKeys("qwerty");
+		//driver.findElement(By.id("email")) .sendKeys("Ftestuser@email.com");
+		driver.findElement(By.xpath("//*[@type='email']")) .sendKeys("Ftestuser@email.com");
+		//driver.findElement (By.id("password")) .sendKeys("qwerty");
+		driver.findElement(By.cssSelector("#password")) .sendKeys("qwerty");
 		driver.findElement(By.xpath("//button[@class='btn btn-primary']")) .click();
-		System.out.println(driver.getTitle());
+		//System.out.println(driver.getTitle());
+		System.out.println("This application name is: "+driver.getTitle());
+	    System.out.println("Create my first automation project and apload it to GitHub");
+			
 		driver.quit();
 		
 		
+	
 		
 	}
 		
